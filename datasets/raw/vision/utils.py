40041a15b49a13f1cd6ae7a8734100e497499fa2
rt-desai <abhyuday.desai@readytensor.com>
2024-03-03 18:17:09 -0800

    updated test key files after fixing class labels; fix appending in train-test_split.csv file for vision

--- datasets/raw/vision/utils.py
+++ datasets/raw/vision/utils.py
@@ -1,40 +1,14 @@
-import os
+import glob
 
 
-RAW_DIR = os.path.join(".", "..", "..", "raw")
-PROCESSED_DIR = os.path.join(".", "..", "..", "processed")
+def get_img_file_count(data_dir):
+    path = f'{data_dir}/**/*.jpg'
+    jpg_files = glob.glob(path, recursive=True)
+    image_count = len(jpg_files)
+    print(image_count)
 
-DATA_DIR = os.path.join(RAW_DIR, "vision", "data")
-AI_GENERATED_DIR_NAMES = ["G01_Photoshop_Generative"]
 
-
-def get_file_names_and_labels():
-    class_labels = [
-        i
-        for i in os.listdir(DATA_DIR)
-        if i.lower().startswith("d") or i.lower().startswith("g")
-    ]
-    class_labels = sorted(class_labels)
-
-    X = []
-    y = []
-
-    for label in class_labels:
-        images_dir_path = os.path.join(DATA_DIR, label, "images")
-
-        sub_dirs = ["flat", "nat"]
-
-        sub_dirs_paths = [os.path.join(images_dir_path, i) for i in sub_dirs]
-        for path in sub_dirs_paths:
-            images_files_names = [
-                i
-                for i in os.listdir(path)
-                if i.lower().endswith(".jpg") or i.lower().endswith(".jpeg")
-            ]
-            images_files_paths = [os.path.join(path, i) for i in images_files_names]
-            images_files_paths = sorted(images_files_paths)
-
-            X.extend(images_files_paths)
-            y += [label] * len(images_files_paths)
-
-    return X, y
+if __name__ == "__main__":
+    data_dir = "./data"
+    data_dir = "./../../processed/vision"
+    get_img_file_count(data_dir)
